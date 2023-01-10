086b1ea057af5e47b7a0b0d252d4910af1475286
Documenter.jl <documenter@juliadocs.github.io>
2023-01-10 10:09:22 +0000

    build based on da8ab4e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Kyuri","category":"page"},{"location":"#Kyuri","page":"Home","title":"Kyuri","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Kyuri.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Kyuri]","category":"page"}]
+}
