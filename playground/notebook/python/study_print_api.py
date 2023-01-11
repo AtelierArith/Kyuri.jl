9c028e7e9374340bab8b2c4b333bc4f63ec3beb6
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2023-01-11 21:48:44 +0900

    update notebook

--- playground/notebook/python/study_print_api.py
+++ playground/notebook/python/study_print_api.py
@@ -3,6 +3,7 @@ import importlib
 from importlib import metadata
 import inspect
 import isort
+import os
 
 # %%
 import quri_parts
@@ -44,9 +45,6 @@ isort.place_module(is_clifford.__module__)
 isort.place_module(dataclass.__module__) == "STDLIB"
 
 # %%
-metadata.distribution("qulacs").version
-
-# %%
 metadata.distribution("quri_parts").version
 
 # %%
@@ -55,7 +53,7 @@ MAX_NEXT_LEVEL = 3
 
 
 # %%
-def print_pymod_api(pymod, level=0):
+def collect_target_objects(pymod):
     if isinstance(pymod, str):
         pymod = importlib.import_module(f"{pymod}")
 
@@ -66,12 +64,12 @@ def print_pymod_api(pymod, level=0):
     alias_functions = []
     for attr_str in sorted(pymod.__dir__()):
         attr = getattr(pymod, attr_str)
-        if inspect.ismodule(attr):
-            continue
         if attr_str.startswith("_"):
+            # do not include private objects
             continue
         if inspect.isclass(attr):
             if isort.place_module(attr.__module__) == "STDLIB":
+                # ignore Python classes that are provided by some Python standard libraries
                 continue
             if attr.__module__ != pymod_name:
                 alias_classes.append(
@@ -81,6 +79,7 @@ def print_pymod_api(pymod, level=0):
             classes.append(attr_str)
         if inspect.isfunction(attr):
             if isort.place_module(attr.__module__) == "STDLIB":
+                # ignore Python functions that are provided by some Python standard libraries
                 continue
             if attr.__module__ != pymod_name:
                 alias_functions.append(
@@ -88,24 +87,49 @@ def print_pymod_api(pymod, level=0):
                 )
                 continue
             functions.append(attr_str)
+    return dict(
+        functions=functions,
+        alias_functions=alias_functions,
+        classes=classes,
+        alias_classes= classes,
+    )
 
+def print_pymod_api(pymod, level=0):
+    if isinstance(pymod, str):
+        pymod = importlib.import_module(f"{pymod}")
+    pymod_name = pymod.__name__
+    ret = collect_target_objects(pymod)
     indent = 4 * level * WHITE_SPACE
     print(indent + f"# --- {pymod_name} ---")
-    for (objs, obj_name) in [
-        (classes, "classes"),
-        (functions, "functions"),
-        (alias_classes, "alias_classes"),
-        (alias_functions, "alias_functions"),
-    ]:
+    for (obj_name, objs) in ret.items():
         print(indent + f"const {pymod_name}_{obj_name} = [")
         for c in objs:
             print(indent + 4 * WHITE_SPACE + ":" + c + ",")
         print(indent + "]")
         print()
+        
+def write_pymod_api(pymod):
+    if isinstance(pymod, str):
+        pymod = importlib.import_module(f"{pymod}")
+    pymod_name = pymod.__name__
+    filename_base = pymod_name.replace(".", "/")
+    print(pymod_name)
+    ret = collect_target_objects(pymod)
+    print(f"# --- {pymod_name} ---")
+    for (obj_name, objs) in ret.items():
+        jlfile_name = filename_base + "_" + obj_name + ".jl"
+        os.makedirs(os.path.dirname(filename_base), exist_ok=True)
+        with open(jlfile_name, "w") as f:
+            f.write(f"# --- {filename_base}_{obj_name}.jl\n")
+            f.write(f"const {pymod_name}_{obj_name} = [\n")
+            for c in objs:
+                f.write(4 * WHITE_SPACE + ":" + c + ",\n")
+            f.write("]\n")
+                                                    
 
 
 # %%
-print_pymod_api("quri_parts.core.operator.pauli")
+write_pymod_api("quri_parts.core.operator.pauli")
 
 
 # %%
@@ -137,8 +161,29 @@ def print_all():
 # %%
 print_all()
 
-# %%
 
 # %%
+def generate_api(pymod="quri_parts", level=0):
+    if isinstance(pymod, str):
+        pymod = importlib.import_module(f"{pymod}")
+    for attr_str in sorted(pymod.__dir__()):
+        attr = getattr(pymod, attr_str)
+        if inspect.ismodule(attr):
+            if isort.place_module(attr.__name__) == "STDLIB":
+                # "ignore standard lib e.g. `re`
+                continue
+            indent = 4 * level * WHITE_SPACE
+            pymod_name = attr.__name__
+            if level + 1 > MAX_NEXT_LEVEL:
+                continue
+            if pymod_name in ["numpy"]:
+                continue
+            #print(indent + f"# --- print-module-{attr.__name__} ---")
+            generate_api(pymod=pymod_name, level=level + 1)
+            write_pymod_api(pymod=pymod_name)
+
+
+# %%
+generate_api("quri_parts")
 
 # %%
